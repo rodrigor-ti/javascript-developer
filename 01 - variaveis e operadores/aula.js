0913d5e290bebd0a36b72194096f41125341ff21
rodrigor-ti <rodrigo.oliveira.rodrigues@gmail.com>
2024-10-29 11:03:41 -0300

    Ajustes e implementações na Aulo 01

--- 01 - variaveis e operadores/aula.js
+++ 01 - variaveis e operadores/aula.js
@@ -1,4 +1,8 @@
-
+// preciso dar CD e ir ate o diretorio do arquivo exemplo: cd '.\01 - variaveis e operadores\'
 let variavel = 10 / 10;
 
 console.log(variavel);
+
+console.log("Imprima algo para teste");
+
+
